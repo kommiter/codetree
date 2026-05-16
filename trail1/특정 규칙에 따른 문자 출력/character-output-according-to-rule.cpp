e8eb091caa2fd5189e6ceac736e0d76b9ff27fc8
Justin Lim <61772869+kommiter@users.noreply.github.com>
2026-05-16 21:05:48 +0900

    [Passed] 특정 규칙에 따른 문자 출력 (8ms, 0MB)

---
+++ trail1/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n; cin >> n;
+    for (int i = 0; i < n; i++){
+        for (int j = n-i-2; j >= 0; j--){
+            cout << "  ";
+        }
+
+        for (int j = 0; j < i+1; j++){
+            cout << "@";
+            if (j != i) cout << " ";
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < n-1; i++){
+        for (int j = n-i-2; j >= 0; j--){
+            cout << "@";
+            if (j != 0) cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
